800ca7b819f032430d918e3779ca62864c9dea94
Rasimali97 <rasimya@code.edu.az>
2023-09-17 13:58:16 +0400

    Hazirdir

--- app.js
+++ app.js
@@ -10,61 +10,55 @@ app.use(bodyParser.json());
 app.use(cors());
 
 let connection = mysql.createConnection({
-    host: "boovkxtqcqckqxmiesaa-mysql.services.clever-cloud.com",
-    user: "ue1klfzskuwvozwt",
-    password: "2BIBqbc36V8rzWHlJcec",
-    database: "boovkxtqcqckqxmiesaa",
-  });
+  host: "boovkxtqcqckqxmiesaa-mysql.services.clever-cloud.com",
+  user: "ue1klfzskuwvozwt",
+  password: "2BIBqbc36V8rzWHlJcec",
+  database: "boovkxtqcqckqxmiesaa",
+});
 
 app.get("/users", function (req, res) {
-    connection.query("select * from users", function (err, result, fields) {
-      res.send(result);
-    });
+  connection.query("select * from users", function (err, result, fields) {
+    res.send(result);
   });
-  
+});
+
 app.get("/users/:id", (req, res) => {
-    const elem = req.params;
-    connection.query("select * from users", function (err, result, fields) {
-      for (let i = 0; i < result.length; i++) {
-        if (elem.id == result[i].id) {
-          res.send(result[i]);
-        }
+  const elem = req.params;
+  connection.query("select * from users", function (err, result, fields) {
+    for (let i = 0; i < result.length; i++) {
+      if (elem.id == result[i].ID) {
+        res.send(result[i]);
       }
-    })
+    }
   })
-  
+})
 app.delete("/users/:id", (req, res) => {
-    const elem = req.params.id;
-    // const silininenElementArray = db.filter(
-    //   (element) => element.id != elem
-    // );
-    connection.query(
-      `DELETE FROM users WHERE id=${elem}`,
-      function (err, result, fields) {
-        console.log(result);
-        res.send(result);
-      }
-    );
-  });
-  
-  app.post("/users/", (req, res) => {
-    let obj = req.body;
-    console.log(obj);
-    connection.query(
-      `INSERT INTO users (id, ad, soyad, password)
-      VALUES ("${obj.id}", "${obj.ad}", "${obj.soyad}", "${obj.password}")`,
-      function (err, result, fields) {
-          console.log(result);
-          app.get("/student", function (req, res) {
-            res.send(result);
-          });
-      }
-    );
-    connection.query("select * from users", function (err, result, fields) {
+  const elem = req.params.id;
+
+  connection.query(
+    `DELETE FROM users WHERE ID=${elem}`,
+    function (err, result, fields) {
       console.log(result);
       res.send(result);
-    });
-  });
-  
-  
-  app.listen(process.env.PORT || 3000);
+    }
+  );
+});
+
+app.post("/users/", (req, res) => {
+  let obj = req.body;
+  console.log(obj);
+  connection.query(
+    `INSERT INTO users (ID, Lastname, Firstname, Passwords) VALUES ("${obj.ID}", "${obj.Lastname}", "${obj.Firstname}", "${obj.Passwords}")`,
+    function (err, result, fields) {
+      res.send(result);
+    }
+  );
+});
+
+  // connection.query("select * from users", function (err, result, fields) {
+  //   console.log(result);
+    // res.send(result);
+  // });
+
+
+app.listen(process.env.PORT || 3000);
